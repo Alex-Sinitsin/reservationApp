84751ecc6e5983adc96ee988a6035ab4abe22b34
Alex-Sinitsin <lehych99@gmail.com>
2021-07-27 23:12:52 +0500

    Добавил модели таблиц для построения схемы БД

--- reservationapp/app/controllers/HomeController.scala
+++ reservationapp/app/controllers/HomeController.scala
@@ -1,24 +1,34 @@
 package controllers
 
+import models._
+
 import javax.inject._
-import play.api._
 import play.api.mvc._
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(
+      protected val dbConfigProvider: DatabaseConfigProvider,
+      val cc: ControllerComponents)(implicit ec: ExecutionContext)
+  extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  val dao = new DBDao(dbConfigProvider)
+
+  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    val f: Future[Unit] = dao.createSchemaIfNotExist
+
+//    dao.createUser(User(-1,"Лиза", "Директор", "liza@mail.ru", "pass"))
+
+    dao.getAllUsers.foreach(println)
+
     Ok(views.html.index())
   }
 }
