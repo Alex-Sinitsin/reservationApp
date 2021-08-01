a7d8e71faf049e28b7186baaaabbd5d17ffd6bc0
Alex-Sinitsin <lehych99@gmail.com>
2021-08-01 22:48:26 +0500

    Добавил метод для работы с url пользователей и настроил http json ответы

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import io.circe.{Decoder, Encoder, parser}
+import io.circe.syntax.EncoderOps
 import models._
 
 import javax.inject._
@@ -10,6 +12,7 @@ import slick.jdbc.JdbcProfile
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
+import io.circe.syntax._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -21,43 +24,23 @@ class HomeController @Inject()(protected val dbConfigProvider: DatabaseConfigPro
 
   val dao = new DatabaseDAO(dbConfigProvider)
 
-  def withJsonBody[A](f: A => Future[Result])(implicit request: Request[AnyContent], reads: Reads[A]): Future[Result] = {
-    request.body.asJson.map { body =>
-      Json.fromJson[A](body) match {
-        case JsSuccess(a, path) => f(a)
-        case e @ JsError(_) => Future.successful(Redirect(routes.HomeController.index()))
-      }
-    }.getOrElse(Future.successful(Redirect(routes.HomeController.index())))
-  }
+  // Circe Json Encoder и Decoder
+  implicit val UserEncoder: Encoder[User] =
+    Encoder.forProduct5("id", "name", "position", "email", "password")(User.unapply(_).get)
+  implicit val UserDecoder: Decoder[User] =
+    Decoder.forProduct5("id", "name", "position", "email", "password")(User.apply)
 
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-
-    Ok(views.html.index())
     //    dao.createSchemaIfNotExist
     //    dao.dropSchemaIfExist
 
-    //    dao.createUser(User(-1,"Лиза", "Директор", "liza@mail.ru", "pass")).onComplete {
-    //      case Success(result) =>  println(result)
-    //      case Failure(exception) => println(exception)
-    //    }
+//    dao.createUser(User(-1, "Елизавета", "HR", "liza@pochta.ru", "password"))
+    Ok(views.html.index())
+  }
 
-    //    dao.updateUser(User(1,"Алексей", "Разработчик", "alex@mail.ru", "pass1word")).onComplete {
-    //      case Success(result) =>  println(result)
-    //      case Failure(exception) => println(exception)
-    //    }
-
-
-//    Ok(views.html.index(Json.toJson(users)))
-
-
-//    withJsonBody[List[User]]{
-//    val users: Future[List[User]] = dao.getAllUsers.map(usl => Ok(Json.toJson(usl)(dao.ListUserWrites)))
-//    }(dao.UserReads)
-
-
-//    withJsonBody[List[User]] {
-//
-//    }(dao.UserReads)
-
+  def users(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+    for {
+      users <- dao.getAllUsers
+    } yield Ok(users.asJson.toString)
   }
 }
