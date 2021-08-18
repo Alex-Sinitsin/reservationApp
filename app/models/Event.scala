78ad73c7b8e085d3db25c1e8ba364bcb0a232fca
Alex-Sinitsin <lehych99@gmail.com>
2021-08-18 15:24:46 +0500

    Дабавил JSON конвертеры

--- app/models/Event.scala
+++ app/models/Event.scala
@@ -1,3 +1,35 @@
 package models
 
-case class Event(id: Int, title: String, orgUserId: Int, members: Option[Seq[User]], itemId: Int, description: Option[String])
+import play.api.libs.functional.syntax.{toFunctionalBuilderOps, unlift}
+import play.api.libs.json.{JsPath, Reads, Writes}
+
+import java.time.{LocalDate, LocalTime}
+import java.util.UUID
+
+case class Event(id: Long, title: String, date: LocalDate, startAt: LocalTime, endAt: LocalTime, orgUserId: UUID, members: Option[List[User]], itemId: Long, description: Option[String])
+
+object Event {
+  implicit val EventReads: Reads[Event] = (
+    (JsPath \ "id").read[Long] and
+      (JsPath \ "title").read[String] and
+      (JsPath \ "date").read[LocalDate] and
+      (JsPath \ "startAt").read[LocalTime] and
+      (JsPath \ "endAt").read[LocalTime] and
+      (JsPath \ "orgUserId").read[UUID] and
+      (JsPath \ "members").readNullable[List[User]] and
+      (JsPath \ "itemId").read[Long] and
+      (JsPath \ "description").readNullable[String]
+    )(Event.apply _)
+
+  implicit val EventWrites: Writes[Event] = (
+    (JsPath \ "id").write[Long] and
+      (JsPath \ "title").write[String] and
+      (JsPath \ "date").write[LocalDate] and
+      (JsPath \ "startAt").write[LocalTime] and
+      (JsPath \ "endAt").write[LocalTime] and
+      (JsPath \ "orgUserId").write[UUID] and
+      (JsPath \ "members").writeNullable[List[User]] and
+      (JsPath \ "itemId").write[Long] and
+      (JsPath \ "description").writeNullable[String]
+    )(unlift(Event.unapply))
+}
