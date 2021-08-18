d6563b517af6bf9350c7855bcb87e6cce021eadb
Alex-Sinitsin <lehych99@gmail.com>
2021-08-18 15:17:08 +0500

    Добавил определения для создания новой схемы БД и таблиц

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -54,6 +54,36 @@ DROP TABLE auth.silhouette_tokens;
 DROP TABLE auth.silhouette_password_info;
 DROP TABLE auth.silhouette_user_login_info;
 DROP TABLE auth.silhouette_login_info;
-DROP TABLE auth.silhouette_users;
+DROP CASCADE TABLE auth.silhouette_users;
 DROP TABLE auth.silhouette_user_roles;
 DROP SCHEMA auth;
+
+# --- !Ups
+CREATE SCHEMA app;
+
+CREATE TABLE app.items (
+  id      BIGSERIAL   NOT NULL PRIMARY KEY,
+  name   VARCHAR      NOT NULL
+);
+
+INSERT INTO app.items (name)
+VALUES ('Переговорная');
+
+CREATE TABLE app.events (
+  id      BIGSERIAL   NOT NULL PRIMARY KEY,
+  title   VARCHAR     NOT NULL,
+  date    DATE        NOT NULL,
+  startAt TIMETZ        NOT NULL,
+  endAt   TIMETZ        NOT NULL,
+  orgUserID UUID      NOT NULL,
+  members JSON,
+  itemID BIGINT         NOT NULL,
+  description VARCHAR,
+  CONSTRAINT events_user_id_fk FOREIGN KEY (orgUserID) REFERENCES auth.silhouette_users (id) ON DELETE CASCADE,
+  CONSTRAINT events_item_id_fk FOREIGN KEY (itemID) REFERENCES app.items (id) ON DELETE CASCADE
+);
+
+# --- !Downs
+DROP TABLE app.events;
+DROP TABLE app.items;
+DROP SCHEMA app
