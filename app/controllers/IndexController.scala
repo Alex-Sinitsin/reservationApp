9b5d2974dc2247b0f3b1f400ef2ff04fc5ad8af4
Alex-Sinitsin <lehych99@gmail.com>
2021-08-25 14:19:15 +0500

    Добавил несколько переменных для теста

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -5,9 +5,10 @@ import play.api.libs.json.{JsString, Json}
 import play.api.mvc._
 import utils.auth.JWTEnvironment
 
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
-import java.time.{LocalDate, LocalTime}
+import java.time.{Duration, LocalDate, LocalDateTime, LocalTime}
 
 /**
  *  Контроллер главной страницы
@@ -20,9 +21,16 @@ class IndexController @Inject() (silhouette: Silhouette[JWTEnvironment], control
                                 (implicit ex: ExecutionContext) extends AbstractController(controllerComponents) {
 
   def index: Action[AnyContent] = silhouette.UnsecuredAction { implicit request: Request[AnyContent] =>
-    val ld = LocalDate.now().toString
-    val lt1 = LocalTime.parse("13:00").toString
-    val lt2 = LocalTime.parse("13:30").toString
-    Ok(Json.obj("ld"->ld, "lt1"->lt1, "lt2"->lt2))
+    val dtf: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+    val ld = LocalDate.now()
+    val lt1 = LocalTime.parse("13:00")
+    val lt2 = LocalTime.parse("13:30")
+
+    val dateTimeOne: LocalDateTime = LocalDateTime.parse("2019-04-28 14:32", dtf).minus(Duration.ofMinutes(1))
+    val dateTimeTwo: LocalDateTime = LocalDateTime.parse("2019-04-28 14:32", dtf)
+
+    val value = dateTimeOne compareTo dateTimeTwo
+
+    Ok(Json.obj("ldt1"->dateTimeOne, "ldt2"->dateTimeTwo, "val"->value))
   }
 }
