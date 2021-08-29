9e1f8e3fa60df67a9ca006e63226f7aa8160345c
Alex-Sinitsin <lehych99@gmail.com>
2021-08-29 20:59:51 +0500

    Добавил проверку роли администратора у авторизованного пользователя события при удалении пользователя

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -17,13 +17,13 @@ import scala.concurrent.{ExecutionContext, Future}
 /**
  * Контроллер для работы с пользователями
  *
- * @param silhouette Стек Silhouette
+ * @param silhouette           Стек Silhouette
  * @param controllerComponents Экземпляр трейта `ControllerComponents`
- * @param eventService Сервис для работы с событиями
- * @param credentialsProvider Провайдер аутентификации по логину/паролю (Silhouette)
- * @param authInfoRepository Репозиторий информации об авторизации (Silhouette)
- * @param hasSignUpMethod Вспомогательная утилита для проверки наличия метода аутентификации
- * @param ex Контекст выполнения
+ * @param eventService         Сервис для работы с событиями
+ * @param credentialsProvider  Провайдер аутентификации по логину/паролю (Silhouette)
+ * @param authInfoRepository   Репозиторий информации об авторизации (Silhouette)
+ * @param hasSignUpMethod      Вспомогательная утилита для проверки наличия метода аутентификации
+ * @param ex                   Контекст выполнения
  */
 class UserController @Inject()(silhouette: Silhouette[JWTEnvironment],
                                controllerComponents: ControllerComponents,
@@ -39,15 +39,21 @@ class UserController @Inject()(silhouette: Silhouette[JWTEnvironment],
    *
    * @return Результат выполнения операции
    */
-  def delete(userID: UUID): Action[AnyContent] = silhouette.SecuredAction(hasSignUpMethod[JWTEnvironment#A](CredentialsProvider.ID)).async { implicit request: SecuredRequest[JWTEnvironment, AnyContent] =>
-    userService.retrieveUserLoginInfo(userID, credentialsProvider.id).flatMap {
-      case Some((user, loginInfo)) =>
-        authInfoRepository.remove[PasswordInfo](loginInfo)
-        userService.delete(userID, user.email).flatMap { delResult =>
-          if(delResult) Future.successful(Ok(Json.obj("success" -> "Пользователь успешно удален!")))
-          else Future.successful(BadRequest(Json.obj("error" -> "Произошла ошибка при удалении пользователя!")))
-        }
-      case None => Future.successful(BadRequest(Json.obj("error" -> "Пользователь не найден!")))
-    }
+  def delete(userID: UUID): Action[AnyContent] = silhouette.SecuredAction(hasSignUpMethod[JWTEnvironment#A](CredentialsProvider.ID)).async {
+    implicit request: SecuredRequest[JWTEnvironment, AnyContent] =>
+
+      request.identity.role match {
+        case Some("Admin") =>
+          userService.retrieveUserLoginInfo(userID, credentialsProvider.id).flatMap {
+            case Some((user, loginInfo)) =>
+              authInfoRepository.remove[PasswordInfo](loginInfo)
+              userService.delete(userID, user.email).flatMap { delResult =>
+                if (delResult) Future.successful(Ok(Json.obj("success" -> "Пользователь успешно удален!")))
+                else Future.successful(BadRequest(Json.obj("error" -> "Произошла ошибка при удалении пользователя!")))
+              }
+            case None => Future.successful(BadRequest(Json.obj("error" -> "Пользователь не найден!")))
+          }
+        case _ => Future.successful(Forbidden(Json.obj("error" -> "Недостаточно прав для выполнения операции!")))
+      }
   }
 }
