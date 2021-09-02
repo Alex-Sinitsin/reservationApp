81abb00d5fd676ca931f801977d4c97bfb48c078
Alex-Sinitsin <lehych99@gmail.com>
2021-09-02 19:16:15 +0500

    Попытка настроить react-admin не увенчалась успехом

--- ui/src/components/BoardAdmin.js
+++ ui/src/components/BoardAdmin.js
@@ -4,12 +4,37 @@ import SideMenu from "./AdminNavigation/SideMenu";
 
 import "../BoardAdmin.css"
 
-import {Admin, Resource} from 'react-admin';
-import UserIcon from '@material-ui/icons/Group';
+import {Admin, Datagrid, EmailField, fetchUtils, List, Resource, TextField} from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
+import Cookies from "js-cookie";
+import authHeader from "../services/auth-header";
+
+const usersList = props => (
+  <List {...props}>
+    <Datagrid rowClick='edit'>
+      <TextField source='id' />
+      <TextField source='name' />
+      <TextField source='postname' />
+      <EmailField source='email' />
+    </Datagrid>
+  </List>
+);
 
 const BoardAdmin = () => {
 
-  const [users, setUsers] = useState()
+  const [users, setUsers] = useState();
+
+  const API_URL = "http://localhost:3000/api";
+  const fetchJson = (url, options = {}) => {
+    if (!options.headers) {
+      options.headers = new Headers({ Accept: 'application/json' });
+    }
+    options.headers.set('Content-Type', 'application/json');
+    options.headers.set('Csrf-Token', Cookies.get('csrfCookie'));
+    options.headers.set('X-Auth-Token', authHeader());
+    return fetchUtils.fetchJson(url, options);
+  };
+  const dataProvider = simpleRestProvider(API_URL, fetchJson);
 
   useEffect(() => {
     document.title = 'Панель администратора';
@@ -21,24 +46,15 @@ const BoardAdmin = () => {
       <div className="container contentWrapper">
         <div className="row">
           <div className="col-md-10">
-            Контент
+            <Admin dataProvider={dataProvider}>
+              <Resource name="users" list={usersList} />
+            </Admin>
           </div>
           <div className="col-md-2">
             <SideMenu />
           </div>
         </div>
       </div>
-      {/*<div className="row">
-        <div className="card card-container col-md-4">
-          <div className="d-flex flex-column bd-highlight mb-3">
-            <div className="p-2 bd-highlight">
-              <Link to={"/register"} className="link">
-                Регистрация пользователей
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>*/}
     </div>
   )
 };
